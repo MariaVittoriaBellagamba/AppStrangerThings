d1a242aa19d6a76d6cf75f2b7c81455e091fe115
Maria Vittoria Bellagamba <mariavittoriabellagamba@hotmail.it>
2021-05-12 12:21:57 +0200

    Modifiche3

--- src/proviamo.java
+++ src/proviamo.java
@@ -5,6 +5,7 @@ public class proviamo {
 	public static void main(String[] args) {
 		System.out.print("ciao a tutti" + add(3,2));
 		System.out.print("daje!");
+		System.out.print("che stress!");
 
 	}
 	
